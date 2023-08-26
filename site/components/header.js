6f4adaf57a39eba5d7d89059ceb9e37856b292cf
Aaron Hung <ahx5x16@gmail.com>
2023-08-26 12:01:39 -0400

    Added add volunteer button

--- site/components/header.js
+++ site/components/header.js
@@ -1,9 +1,14 @@
 import styles from './header.module.css';
 
+function newProfile () {
+
+}
+
 export default function Header () {
     return (
         <header className={styles.header}>
             <h1 className={styles.titleName}>HaHa Heroes</h1>
+            <button onClick={newProfile} className={styles.newButton}>Add Volunteer</button>
         </header>
     );
 }
