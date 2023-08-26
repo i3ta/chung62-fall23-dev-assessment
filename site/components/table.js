e930cb9209ae36d3531a570085c9c7131a09fe78
Aaron Hung <ahx5x16@gmail.com>
2023-08-26 11:13:11 -0400

    Formatted and styled table

--- site/components/table.js
+++ site/components/table.js
@@ -6,13 +6,15 @@ function Row ({ rowData }) {
 
     // Return table row
     return (
-        <tr>
-            <td>{name}</td>
-            <td>
-                <img src={avatar} alt={`Picture of ${name}`} />
+        <tr className={(id % 2 ? styles.rowEven : styles.rowOdd)}>
+            <td className={styles.avatarContainer}>
+                <img src={avatar} alt={`Picture of ${name}`} className={styles.avatar} />
             </td>
+            <td>{name}</td>
             <td>{phone}</td>
-            <td>{email}</td>
+            <td>
+                <a href={`mailto:${email}`} className={styles.email}>{email}</a>
+            </td>
             <td>{rating}</td>
             <td>{(status ? 'Active' : 'Inactive')}</td>
             <td>{hero_project}</td>
@@ -24,15 +26,15 @@ export default function Table ({ tableData }) {
     console.log('created table');
     return (
         <table className={styles.table}>
-            <thead>
+            <thead className={styles.header}>
                 <tr>
-                    <th>Name</th>
                     <th>Avatar</th>
+                    <th>Name</th>
                     <th>Phone</th>
                     <th>Email</th>
                     <th>Rating</th>
                     <th>Status</th>
-                    <th>Hero Project</th>
+                    <th>Project</th>
                 </tr>
             </thead>
             <tbody>
