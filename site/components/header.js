c85f7c449c570700a778843da94d2ce14f824ada
Aaron Hung <ahx5x16@gmail.com>
2023-08-26 11:48:04 -0400

    Added header formatting

--- site/components/header.js
+++ site/components/header.js
@@ -2,6 +2,8 @@ import styles from './header.module.css';
 
 export default function Header () {
     return (
-        <h1>HaHa Heroes</h1>
+        <header className={styles.header}>
+            <h1 className={styles.titleName}>HaHa Heroes</h1>
+        </header>
     );
 }
